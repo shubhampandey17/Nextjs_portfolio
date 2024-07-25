5d366daeb684a8a9f36a43a439ec5eaaa00b5eb0
Shubham pandey <94579821+shubhampandey17@users.noreply.github.com>
2024-07-25 21:28:31 +0530

    Update route.js

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,12 +1,16 @@
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
+import { NextResponse } from 'next/server';
+import { Resend } from 'resend';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req, res) {
+export async function POST(req) {
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
+
   try {
     const data = await resend.emails.send({
       from: fromEmail,
@@ -21,8 +25,9 @@ export async function POST(req, res) {
         </>
       ),
     });
+
     return NextResponse.json(data);
   } catch (error) {
-    return NextResponse.json({ error });
+    return NextResponse.json({ error: error.message });
   }
 }
